f8b05896a248ba0560c3c1813d36c997f97dae2d
Margo.Raja <margo.raja@guardtime.com>
2016-12-12 16:22:16 +0200

    Adjusted exception message.

--- ksi-common/src/test/java/com/guardtime/ksi/CommonTestUtil.java
+++ ksi-common/src/test/java/com/guardtime/ksi/CommonTestUtil.java
@@ -33,7 +33,7 @@ public class CommonTestUtil {
     public static InputStream load(String file) throws FileNotFoundException {
         InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(file);
         if (stream == null) {
-            throw new FileNotFoundException(file + "(You need to add integrationtest.properties file to folder 'ksi-api/src/test/resources' to run integration tests)");
+            throw new FileNotFoundException(file + "(integrationtest.properties file must be added to folder 'ksi-api/src/test/resources' for running the integration tests)");
         }
         return stream;
     }

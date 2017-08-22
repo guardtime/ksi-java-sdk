0f8ee9e8375b1da69d58709875b97fee7216b6b0
Margo.Raja <margo.raja@guardtime.com>
2017-08-22 15:41:10 +0300

    Made added apache clients public as supposed to be.

--- ksi-service-client-apache-http/src/main/java/com/guardtime/ksi/service/client/http/apache/ApacheHttpExtenderClient.java
+++ ksi-service-client-apache-http/src/main/java/com/guardtime/ksi/service/client/http/apache/ApacheHttpExtenderClient.java
@@ -37,7 +37,7 @@ public class ApacheHttpExtenderClient extends AbstractApacheHttpClient implement
      *
      * @param settings - Settings defined by {@link com.guardtime.ksi.service.client.http.CredentialsAwareHttpSettings}
      */
-    ApacheHttpExtenderClient(CredentialsAwareHttpSettings settings) {
+    public ApacheHttpExtenderClient(CredentialsAwareHttpSettings settings) {
         super(settings);
         this.settings = settings;
     }
@@ -48,7 +48,7 @@ public class ApacheHttpExtenderClient extends AbstractApacheHttpClient implement
      * @param settings - Settings defined by {@link com.guardtime.ksi.service.client.http.HttpSettings}
      * @param asyncConfiguration - Configuration defined by an instance of {@link ApacheHttpClientConfiguration}
      */
-    ApacheHttpExtenderClient(CredentialsAwareHttpSettings settings, ApacheHttpClientConfiguration asyncConfiguration) {
+    public ApacheHttpExtenderClient(CredentialsAwareHttpSettings settings, ApacheHttpClientConfiguration asyncConfiguration) {
         super(settings, asyncConfiguration);
         this.settings = settings;
     }

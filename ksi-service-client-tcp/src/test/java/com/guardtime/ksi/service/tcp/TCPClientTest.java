80d60a1bf453427107471206a8768ae11f5c34c7
Tarmo Kalling <tarmo.kalling@guardtime.com>
2017-09-12 13:18:58 +0300

    Refactored TCPClientTest a bit

--- ksi-service-client-tcp/src/test/java/com/guardtime/ksi/service/tcp/TCPClientTest.java
+++ ksi-service-client-tcp/src/test/java/com/guardtime/ksi/service/tcp/TCPClientTest.java
@@ -9,27 +9,32 @@ import java.io.ByteArrayInputStream;
 
 public class TCPClientTest {
 
+    private static final TCPClientSettings TCP_SETTINGS_ANON_PDUV1 = createTCPSettings("anon", PduVersion.V1);
+    private static final TCPClientSettings TCP_SETTINGS_ANON_PDUV2 = createTCPSettings("anon", PduVersion.V2);
+    private static final TCPClientSettings TCP_SETTINGS_ANON2_PDUV1 = createTCPSettings("anon2", PduVersion.V1);
+
     @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp =
             "TCPClient.signingSettings.pduVersion and TCPClient.extendingSettings.pduVersion must match. " +
                     "Use SigningTCPClient and ExtenderTCPClient if they do not match")
     public void testPduVersionsMismatch() {
-        new TCPClient(new TCPClientSettings("tcp://0.0.0.0:0", 10, new KSIServiceCredentials("anon", "anon"), PduVersion.V1),
-                new TCPClientSettings("tcp://0.0.0.0:0", 10, new KSIServiceCredentials("anon", "anon"), PduVersion.V2));
+        new TCPClient(TCP_SETTINGS_ANON_PDUV1, TCP_SETTINGS_ANON_PDUV2);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp =
             "TCPClient.signingSettings.serviceCredentials and TCPClient.extendingSettings.serviceCredentials must match. " +
                     "Use SigningTCPClient and ExtenderTCPClient if they do not match")
     public void testServiceCredentialsMismatch() {
-        new TCPClient(new TCPClientSettings("tcp://0.0.0.0:0", 10, new KSIServiceCredentials("anon1", "anon1"), PduVersion.V1),
-                new TCPClientSettings("tcp://0.0.0.0:0", 10, new KSIServiceCredentials("anon2", "anon2"), PduVersion.V1));
+        new TCPClient(TCP_SETTINGS_ANON_PDUV1, TCP_SETTINGS_ANON2_PDUV1);
     }
 
-    @Test(expectedExceptions = KSIClientException.class, expectedExceptionsMessageRegExp = "Extender connection is not configured.*")
+    @Test(expectedExceptions = KSIClientException.class, expectedExceptionsMessageRegExp =
+            "Extender connection is not configured.*")
     public void testExtendingIfOnlySigningIsConfigured() throws Exception {
-        TCPClient tcpClient = new TCPClient(
-                new TCPClientSettings("tcp://0.0.0.0:0", 10, new KSIServiceCredentials("anon", "anon"), PduVersion.V1));
-        tcpClient.extend(new ByteArrayInputStream(new byte[]{0}));
+        TCPClient tcpClient = new TCPClient(TCP_SETTINGS_ANON_PDUV1);
+        tcpClient.extend(new ByteArrayInputStream(new byte[] {0}));
     }
 
+    private static TCPClientSettings createTCPSettings(String userPass, PduVersion pduVersion) {
+        return new TCPClientSettings("tcp://0.0.0.0:0", 10, new KSIServiceCredentials(userPass, userPass), pduVersion);
+    }
 }
